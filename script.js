89c47b091c1a07004d32a102e47eaae1ae315954
VictorGuilhermeC <67566818+VictorGuilhermeC@users.noreply.github.com>
2025-04-28 19:58:27 -0300

    Remove script.js comments and "Hello, World!"

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello, World!");
-
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3);
 
@@ -50,18 +48,3 @@ function playGame() {
 }
 
 playGame();
-/*
-Step 6: Write the logic to play the entire game
-Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
-
-Create a new function named playGame.
-Move your playRound function and score variables so that they’re declared inside of the new playGame function
-Play 5 rounds by calling playRound 5 times.
-Hint: When you assign a function call to a variable, the return value of that function is assigned to the variable. Accessing the variable afterward will only provide the assigned value; it doesn’t recall the function. You need to recall the choice functions to get new choices for each round.
-Re-work your previous functions or create more helper functions if necessary. Specifically, you may want to change the return values to something more useful.
-If you already know about loops, you can use them. If not, don’t worry! Loops will be covered in the next lesson.
-*/
-
-/*
-
-*/
